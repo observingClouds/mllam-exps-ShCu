4fe2f1470cb20556b9bf09f4617d51f42e8f6dc5
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2025-06-23 10:40:16 +0200

    successful graph and dataset generation (now with xyz)

--- src/create_state_dataset.py
+++ src/create_state_dataset.py
@@ -7,13 +7,22 @@ a small noise is added to the land-sea mask.
 import xarray as xr
 import numpy as np
 import argparse
+from intake import open_catalog
 
 parser = argparse.ArgumentParser(description="Create a static dataset for neural-lam.")
 parser.add_argument("--input", type=str, help="Path to the input Zarr dataset.", default="../data/test.zarr")
 parser.add_argument("--output", type=str, help="Path to the output Zarr dataset.", default="../data/lsm_DOM02.zarr")
 args = parser.parse_args()
 
-ds = xr.open_zarr(args.input)
+cat = open_catalog("https://raw.githubusercontent.com/observingClouds/eurec4a-intake/refs/heads/remove/compression/catalog.yml")
+
+ds = cat.simulations.ICON.LES_CampaignDomain_control.surface_DOM02.to_dask()
+
+
+#ds = xr.open_zarr(args.input)
 r=np.random.randn(len(ds.cell))/1e3
 
-(xr.where(ds.t_seasfc.isel(time=0).isnull(), 0., 1.)*r).rename("lsm").drop_vars("time").squeeze(drop=True).to_zarr(args.output, mode="w", consolidated=True)
+
+ds_out = ds[['cell']]
+ds_out['lsm'] = (xr.where(ds['t_seasfc'].isel(time=0).isnull(), 0., 1.)*r).rename("lsm")
+ds_out.drop_vars("time").squeeze(drop=True).to_zarr(args.output, mode="w", consolidated=True)
